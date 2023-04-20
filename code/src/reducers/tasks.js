f98ccbc35aed6d4524a64dfc42aee661bbb2f4fe
Frid Matilda <matilda.x.frid@scania.com>
2023-04-20 16:57:35 +0200

    pushy

--- code/src/reducers/tasks.js
+++ code/src/reducers/tasks.js
@@ -34,11 +34,33 @@ export const tasks = createSlice({
       store.items = [...store.items, action.payload];
     },
     removeItem: (store, action) => {
+      const id = action.payload;
+      console.log('action.payload', action.payload, 'id', id);
+      const newArray = store.items;
+
+      const condition = (element) => element.id === id;
+      console.log('condition', condition)
+
+      // The findIndex() method is then used to find the index of the Pokemon object to be deleted from the copied array.
+      // This is done by passing a callback function that checks if an element's id property matches the id value obtained from the action parameter.
+
+      const foundIndex = newArray.findIndex(condition);
+      console.log('foundIndex', foundIndex)
+
+      // Finally, the splice() method is used to remove the Pokemon object from the copied array by passing the index of the Pokemon object and a count of 1 as arguments.
+      // The copied array is then assigned back to the store.items property to update the store state.
+
+      newArray.splice(foundIndex, 1)
+      console.log('newArray.splice(foundIndex, 1)', newArray.splice(foundIndex, 1))
+
       // Here we want to filter out the items
       // And return a new state without said item
-      store.items = store.items.filter((item) => item.id !== action.payload.id);
+      // Filter should not be used here. It is recommended to use splice instead to remove stuff.
+      // Filter is better when filtering based on state, when we want to access the original also
+      // store.items = store.items.filter((item) => item.id !== action.payload.id);
       // console.log('store:', store);
       // console.log('action.payload:', action.payload);
+      //
     },
     removeAll: (store) => {
       // Im not mutating this
